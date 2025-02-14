3864bade492dc42cf43006540c13a3ddedb914ad
Akatsuki15 <albita922003@gmail.com>
2025-02-14 13:12:04 +0100

    solución de problemas

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -12,7 +12,7 @@ function Register() {
         surname: "",
         email: "",
         password: "",
-        accepNotifications: false,
+        acceptNotifications: false,
       });
       const [errors, setErrors] = useState<Record<string, string | undefined>>({});
       const [loading, setLoading] = useState(false);
@@ -76,9 +76,9 @@ function Register() {
         <div className="flex items-center h-5">
           <input
             id="acceptNotifications"
-            name="accepNotifications"
+            name="acceptNotifications"
             type="checkbox"
-            value={form.accepNotifications ? "on" : "off"}
+            value={form.acceptNotifications ? "on" : "off"}
             onChange={handleChangeCheckbox}
             className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
           />
